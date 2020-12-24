eb6737446ae50877a9b8c68b226a7337e0b54028
Christine Ross <christinejoyross@gmail.com>
2020-12-23 19:46:22 -0800

    custom authentication middleware

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
-import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -12,15 +14,17 @@ connectDB()
 
 const app = express();
 
+app.use(express.json())
+
 
 app.get("/", (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
