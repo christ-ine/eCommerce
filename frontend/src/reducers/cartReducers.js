021cb5c3158460b7f6ba1001038aef7fcecbec31
Christine Ross <christinejoyross@gmail.com>
2021-01-06 15:16:23 -0800

    Shipping Screen, save address, and reducers

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,21 +1,21 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/CartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/CartConstants'
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
-    switch(action.type) {
+export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
+    switch (action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
 
             const existItem = state.cartItems.find(x => x.product === item.product)
 
-            if(existItem) {
+            if (existItem) {
                 return {
                     ...state,
-                    cartItems: state.cartItems.map((x) => 
-                    x.product === existItem.product ? item: x )
+                    cartItems: state.cartItems.map((x) =>
+                        x.product === existItem.product ? item : x)
                 }
             } else {
                 return {
-                    ...state, 
+                    ...state,
                     cartItems: [...state.cartItems, item]
 
                 }
@@ -25,7 +25,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                 ...state,
                 cartItems: state.cartItems.filter((x) => x.product !== action.payload)
             }
-        default: 
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return {
+                ...state,
+                shippingAddress: action.payload,
+            }
+        default:
             return state
     }
 }
