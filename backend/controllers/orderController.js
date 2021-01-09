5e4b86e920349dca83eabc4b56a39203e91453f2
Christine Ross <christinejoyross@gmail.com>
2021-01-08 19:08:38 -0800

    order pay reducer and action

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -51,6 +51,31 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
+//@desc update order to paid
+//@route GET /api/orders/:id/pay
+//@access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+    if(order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: erq.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order Not Found')
+    }
+})
+
 export {
-    addOrderItems, getOrderById
+    addOrderItems, getOrderById, updateOrderToPaid
 }
